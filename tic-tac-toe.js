83f8fa06b55ce631b5dabccb83f64e792cd7f5d1
IkobiLynch <82119066+IkobiLynch@users.noreply.github.com>
2022-10-13 14:30:27 -0500

    Changed style when mouse moved over a square &check for the winner and update the status

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,6 +5,9 @@ window.onload = function(){
     var board = document.getElementById("board")
     var box = board.querySelectorAll('div')
 
+    var win = document.getElementById("status")
+    var winner = ""
+
 
     let counter = 0;
 
@@ -55,6 +58,117 @@ window.onload = function(){
         });
 
         
+        //Mouse event listener that changes div class to hover when mouse is over div 
+        item.addEventListener("mouseover", function(){
+            item.setAttribute("class", "hover");
+        });
+
+        //MOuse event listener that returns div to it's previous class
+        item.addEventListener("mouseout", function(){
+            
+            if(item.innerHTML == "X"){
+                item.setAttribute("class", "square X")
+            }
+            else if(item.innerHTML == "O"){
+                item.setAttribute("class", "square O")
+            }
+            else{
+                item.setAttribute("class", "square")
+            }
+        
+        });
+
+
+        let row_win = false;
+        let col_win = false;
+        let diagonal_win = false;
+
+        board.onclick = function(){
+
+            if(counter>4){
+                //For loop to check each row
+                for(var row_check = 0; row_check <= 6; row_check+=3){
+                    //checks if rows are equal and in correct order
+                    if(game_state[row_check] == game_state[row_check+1] && 
+                        game_state[row_check+1] == game_state[row_check+1] &&
+                        game_state[row_check+1] == game_state[row_check+2])
+                        {
+                            //checks that no div has an innerHtml of ""
+                            if(game_state[row_check]!="" || game_state[row_check+1]!="" || game_state[row_check+1]!=""){
+                                winner = game_state[row_check];
+                                row_win = true;
+                            }
+
+                        }
+
+                }
+
+                //For loop to check each column
+
+                for(var col_check = 0; col_check<=3; col_check++)
+                {
+                    if(game_state[col_check] == game_state[col_check+3] &&
+                        game_state[col_check] == game_state[col_check+6] &&
+                        game_state[col_check+3] == game_state[col_check+6])
+                        {
+                            if(game_state[col_check]!="" || game_state[col_check+1]!="" || game_state[col_check+1]!=""){
+                                winner = game_state[col_check];
+
+                                col_win = true;
+                            }
+
+                        }
+
+                }
+
+
+
+                //check if player woon from bottom left to top right
+                if(game_state[2] == game_state[4] && 
+                    game_state[2]== game_state[6] && 
+                    game_state[4] == game_state[6])
+                    {
+                        winner = game_state[2];
+
+                        diagonal_win = true;
+                    }
+
+
+                //check if player won from top left to bottom right
+                if(game_state[0] == game_state[4] && 
+                    game_state[0]== game_state[8] && 
+                    game_state[4] == game_state[8])
+                    {
+                        winner = game_state[0];
+
+                        diagonal_win = true;
+                    }
+
+
+            }
+
+            //Checks if draw conditions have been met by ensuring
+            //all indexes of game_state is filled & no found winning combination
+
+            if(!row_win && !col_win && diagonal_win && !game_state.includes(""))
+            {
+                
+                win.innerHTML = "Draw"
+                win.setAttribute("class", "you-won")
+
+            }
+
+            //Checks if there is a winning condition. 
+            if(row_win || col_win || diagonal_win)
+            {
+                //change div with class status inner html
+                win.innerHTML = "Congratulations! "+ winner +" is the Winner!";
+                //set attribute of win
+                win.setAttribute("class", "you-won");
+                
+            }
+        }
+
     });
 
 
